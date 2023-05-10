fc501a3434bb6e7d50ef40f351ebebf09b6e3df3
Nithya <tryph101@hotmail.com>
2023-05-10 22:56:35 +0800

    Add files via upload

--- RouteSwitch.js
+++ RouteSwitch.js
@@ -1,15 +1,22 @@
-import {Routes, Route } from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Header from './Header';
 import Home from './components/pages/Home'
 import Shop from './components/pages/Shop'
 
 
-function RouteSwitch () {
-  return (
-      <Routes>
-        <Route path = '/' element={<Home />} />
-        <Route path = '/shop' element={<Shop />} />
-      </Routes>
-  )
-};
+function RouteSwitch() {
+    return (
+      <Router>
+        <Header />
+        <Routes>
+          <Route path = '/' element={<Home />} />
+          <Route path = '/shop' element={<Shop />} />
+        </Routes>
+      </Router>
 
-export default RouteSwitch;
+    );
+  }
+
+
+
+export default RouteSwitch
