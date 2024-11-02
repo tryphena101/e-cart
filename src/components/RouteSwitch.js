9559461e62a5c1fa10b1141056f67b7b9f5a9d55
tryphena101 <tryph101@hotmail.com>
2024-11-03 03:08:09 +0800

    Changed again

--- src/components/RouteSwitch.js
+++ src/components/RouteSwitch.js
@@ -1,5 +1,5 @@
 // import {BrowserRouter as Router, Routes, Route, redirect } from 'react-router-dom'
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './Header';
 import Home from './components/pages/Home'
 import Shop from './components/pages/Shop'
@@ -7,13 +7,13 @@ import Shop from './components/pages/Shop'
 
 function RouteSwitch() {
     return (
-      <HashRouter>
+      <BrowserRouter>
         <Header />
         <Routes>
           <Route exact path = '/' element={<Home />}/>
           <Route path = '/shop' element={<Shop />} />
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
 
     );
   }
